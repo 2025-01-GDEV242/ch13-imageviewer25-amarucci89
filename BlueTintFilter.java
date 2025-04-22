d38ea032ddad409f7dd31d67d0ff943626dad8fc
amarucci89 <alexmarucci89@gmail.com>
2025-04-21 21:25:55 -0400

    Added Green Tint Filter class

---
+++ BlueTintFilter.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+
+/**Green Tint Filter
+ * An image filter that tints the image green.
+ *
+ * @author Alessandro Marucci
+ * @version 04.21.2025
+ */
+public class BlueTintFilter extends Filter
+{
+    /**
+     * Constructor for objects of class BlueTintFilter.
+     * @param name The name of the filter.
+     */
+    public BlueTintFilter(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    {
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Color pix = image.getPixel(x, y);
+                int avg = (pix.getRed() + pix.getGreen() + pix.getBlue()) / 3;
+                image.setPixel(x, y, new Color(avg, avg, avg));
+                
+                Color newColor = new Color(0, 255, 0);
+                image.setRGB(x, y, newColor.getRGB());
+            }
+        }
+    }
+}
